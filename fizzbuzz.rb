0d1cccc9858d9b562e92a442b26d67db385daaba
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-27 18:49:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,6 @@
 # See README.md for instructions on how to do this
 
 
-def fizzbuzz(x)
-  if (x % 5 == 0 && x % 3 == 0)
-    puts  "FizzBuzz"
-  elsif (x % 3 == 0)
-    puts "Fizz"
-  elsif (x % 5 == 0)
-    puts "Buzz"
-  else
-    puts nil
+def fizzbuzz()
+
 end
